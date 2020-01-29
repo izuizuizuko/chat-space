9c78a08f57604c00b0206c168a4fb41c1f79ede4
izuizuizuko <izuizuizuko426@gmail.com>
2020-01-29 17:01:49 +0900

    delete  console

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -81,7 +81,6 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.chat-main__list__box:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
